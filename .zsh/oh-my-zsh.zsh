29cc44e1c2c98fc0488a861b6e39898a6269a3bb
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2013-12-05 11:04:40 +0100

    Update zsh plugins

--- .zsh/oh-my-zsh.zsh
+++ .zsh/oh-my-zsh.zsh
@@ -20,6 +20,6 @@ COMPLETION_WAITING_DOTS="true"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # plugins=(thibaudgg)
-plugins=(rails4 heroku gem git brew thibaudgg)
+plugins=(rails heroku gem git brew thibaudgg osx)
 
 . $ZSH/oh-my-zsh.sh
